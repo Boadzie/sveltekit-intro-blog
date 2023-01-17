c32c8793082c331e8a9fd711c3b996676582363f
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-01-17 13:13:09 +0000

    Remove navbar and text from home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,7 @@
 export let data;
 </script>
 <section class="container mx-auto px-4 py-4">
-<h1 class="text-4xl text-blue-400">Welcome to SvelteKit</h1>
-<p class="text-lg my-3">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a class="underline text-blue-500"  href="/about">About page</a>
+
 <div class="grid lg:grid-cols-3 gap-4 ">
   {#each data.posts as post }
     <div class="rounded-sm bg-slate-50 p-5">
