13d87a4d344d1aac6080ae6b24cbc833a6dae66b
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-01-17 13:12:42 +0000

    Add navbar to layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
 	import '../app.postcss';
 </script>
 
+<nav class="p-4 text-white bg-slate-700 display flex items-center justify-start gap-4">
+ <a class="font-bold text-lg hover:text-blue-500" href="/">SvelteKit Intro Blog</a>
+ <a class="font-bold text-lg  hover:text-blue-500"  href="/about">About page</a>
+</nav>
+
+<main>
 <slot />
+</main>
