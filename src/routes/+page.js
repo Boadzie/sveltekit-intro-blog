a8956a0465fe5fce09dcb2b2d39c2d93be1292ad
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-01-23 10:58:38 +0000

    Destructure fetch from load function on page.js

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,5 @@
 /** @type {import('./$types').PageLoad} */
-export async function load() {
+export async function load({ fetch }) {
 	const res = await fetch('https://jsonplaceholder.typicode.com/posts?limit=10');
 	const posts = res.json();
 	return {
